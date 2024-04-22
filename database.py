0020f1c248e998f2c5b564302071e0502e25a804
Ankit Kumar <ankitku4281@gmail.com>
2024-04-22 13:12:42 +0530

    first commit

---
+++ database.py
@@ -0,0 +1,54 @@
+
+from sqlalchemy import create_engine 
+from sqlalchemy.orm import sessionmaker 
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text
+from datetime import datetime
+
+Base = declarative_base()
+
+class Chart(Base):
+    __tablename__ = "charts"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    path = Column(String(255))
+    created_on = Column(DateTime, default=datetime.now)
+
+class Report(Base):
+    __tablename__ = "reports"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    description = Column(Text)
+    created_at = Column(DateTime, default= datetime.now)
+    
+class Dataset(Base):
+    __tablename__ = "datasets"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    description = Column(Text)
+    created_at = Column(DateTime, default= datetime.now)
+    
+class Analysis(Base):
+    __tablename__ = "analysis"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    path = Column(String)
+    data_source_id = Column(Integer, ForeignKey('datasets.id'))
+    created_at = Column(DateTime, default=datetime.now())
+
+# utility functions
+def get_db():
+    engine = create_engine('sqlite:///apad.db')
+    Base.metadata.create_all(engine)
+    return sessionmaker(bind=engine)
+
+def save_to_db(object):
+    db = get_db()    
+    db.add(object)
+    db.commit()
+    db.close()
+
+# create database
+if __name__ == "__main__":
+    engine = create_engine('sqlite:///apad.db')
+    Base.metadata.create_all(engine)
